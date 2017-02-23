a53827bc3e1fd0983a43a9258e76c3a00d794318
Keaton Burleson <keaton.burleson@me.com>
2017-02-23 14:52:54 -0600

    Changed CPU tests to use checkmarks

--- sunshine.rb
+++ sunshine.rb
@@ -24,6 +24,31 @@ sseTest = system('cat /proc/cpuinfo | grep sse').passfail
 sse2Test = system('cat /proc/cpuinfo | grep sse2').passfail
 sse3Test = system('cat /proc/cpuinfo | grep ssse3').passfail
 
+  if mmxTest == "PASS"
+    mmxTest = "fa fa-check green"
+  else
+    mmxTest = "fa fa-times red"
+  end
+
+  if sseTest == "PASS"
+    sseTest = "fa fa-check green"
+  else
+    sseTest = "fa fa-times red"
+  end
+  if sse2Test == "PASS"
+    sse2Test = "fa fa-check green"
+  else
+    sse2Test = "fa fa-times red"
+  end
+
+  if sse3Test == "PASS"
+    sse3Test = "fa fa-check green"
+  else
+    sse3Test = "fa fa-times red"
+  end
+
+
+
 memTestAmt = (getFreeMemory * 0.7).floor
 
 memoryStatus = Tempfile.new('memStatus')
