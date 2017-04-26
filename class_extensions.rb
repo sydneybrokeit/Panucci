9dfe10c97824c3011446045713abc35128633610
Harold Schreckengost <harold@haroldmschreckengost.com>
2017-04-26 10:01:54 -0500

    Pulled a few class definitions out of main, fixed variable issues.

---
+++ class_extensions.rb
@@ -0,0 +1,23 @@
+class TrueClass
+    def passfail
+        'PASS'
+    end
+end
+
+class FalseClass
+    def passfail
+        'FAIL'
+    end
+end
+
+class Hash
+    def nested_each_pair
+        each_pair do |k, v|
+            if v.is_a?(Hash)
+                v.nested_each_pair { |k, v| yield k, v }
+            else
+                yield(k, v)
+            end
+        end
+    end
+end
